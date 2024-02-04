58ede643d9f5f7c1fd75334a936c2168b6849b7f
AbdelrahmanMo7 <abdullrahman.mohamed2358@gmail.com>
2024-02-04 23:13:06 +0200

    AM_V16 :  Create Folders

---
+++ A.M.A/Report_Page.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cafffe_Sytem.A.M.A
+{
+    public partial class Report_Page : Form
+    {
+        public Report_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Report_Page_Load(object sender, EventArgs e)
+        {
+            //Report_Page p = new Report_Page();
+            int x = 2;
+
+        }
+    }
+}
