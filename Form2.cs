ca3d3731844d705477fe7c46838b1f96a1c73870
AbdelrahmanMo7 <abdullrahman.mohamed2358@gmail.com>
2024-02-06 21:43:20 +0200

    AM_V19

--- Form2.cs
+++ Form2.cs
@@ -16,5 +16,48 @@ namespace Cafffe_Sytem
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Aqua;
+            button2.Text = " save ";
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.BurlyWood;
+            button2.Text = " Update ";
+
+        }
+
+        private void button2_TextChanged(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Red;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Cafffe_Sytem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
